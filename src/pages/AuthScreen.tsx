adf5a90a3217cb347127ed895cec087a6dda9983
JuniorFixHow <robertjuniorannan@gmail.com>
2024-07-16 06:36:42 +0100

    fixed scrolls

--- src/pages/AuthScreen.tsx
+++ src/pages/AuthScreen.tsx
@@ -7,7 +7,7 @@ function AuthScreen() {
         <div className="hidden px-16 items-center justify-center flex-col  lg:flex w-1/2 h-full">
             <img className="w-full" src='/imgs/undraw_online_test_re_kyfx.svg' alt="welcome logo" />
             <div className="flex mt-8 flex-col">
-                <span className='font-semibold text-2xl' >Welcome back to Prudential!</span>
+                <span className='font-semibold text-2xl overflow-y-hidden' >Welcome back to Prudential!</span>
                 <span className="text-[0.8rem] text-gray-400" >Sign in and manage your employee data</span>
             </div>
         </div>
